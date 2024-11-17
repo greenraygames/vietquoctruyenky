3eea4c16fdacf419a01e1c226af242e633835fd4
chanmyse-git <chanmyse@gmail.com>
2024-11-17 17:36:59 +0700

    Update request.js

--- site/request.js
+++ site/request.js
@@ -1,7 +1,7 @@
 function Request(uid, stringDat)
 {
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://127.0.0.1:8111/", true);
+  xhr.open("POST", `http://${serverAddress}:${serverPort}/`, true);
   xhr.setRequestHeader('Content-Type', 'text/plain');
   xhr.onreadystatechange = function() 
             {
