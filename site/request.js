07204ffa4c376bec6f217ca988b1b0464055d7c3
chanmyse-git <chanmyse@gmail.com>
2024-11-17 16:36:35 +0700

    Update request.js

--- site/request.js
+++ site/request.js
@@ -3,4 +3,20 @@ function Request(uid, data)
   const xhr = new XMLHttpRequest();
   xhr.open("POST", '127.0.0.1:8111', true); //đổi ip, port ở đây cho hợp với server.
   xhr.setRequestHeader('Content-Type', 'text/plain');
+
+  xhr.onreadystatechange = function() 
+  {
+    if (xhr.readyState === XMLHttpRequest.DONE)
+      {
+        if (xhr.status === 200) 
+        {
+           return xhr.responseText;
+        }
+        else 
+        {
+          alert("Error: " + xhr.status);
+        }
+      }
+  };  
+  xhr.send(message);
 }
