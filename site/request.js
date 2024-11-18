7f8e4e3727df87515b93d71d733a04bf49f5960e
greenraygames <greenraygames@gmail.com>
2024-11-18 07:15:36 +0700

    Update request.js

--- site/request.js
+++ site/request.js
@@ -1,5 +1,7 @@
 function Request(uid, stringDat)
 {
+  try
+  {
   const xhr = new XMLHttpRequest();
   xhr.open("POST", `http://${serverAddress}:${serverPort}/`, true);
   xhr.setRequestHeader('Content-Type', 'text/plain');
@@ -18,4 +20,9 @@ function Request(uid, stringDat)
                 }
             };
             xhr.send(uid + sss + stringDat);
+  }
+  catch(error)
+  {
+    console.log(error.message);
+  }
 }
