c9cedc7959907950e2de3e4b4753b86b48e0d799
chanmyse-git <chanmyse@gmail.com>
2024-11-22 13:11:52 +0700

    Update cookiehandler.js

--- site/cookiehandler.js
+++ site/cookiehandler.js
@@ -1,4 +1,18 @@
-function SetTempCookie(cookieID, cookieValue)
+function SetTempCookie(cookieID, cookieValue) 
 {
-  document.cookie =  cookieID + "=" + cookieValue;
+  document.cookie = `${cookieID}=${cookieValue}; path=/`;
+}
+
+function GetCookieValue(cookieID) 
+{
+  const cookies = document.cookie.split('; ');
+  for (const cookie of cookies) 
+  {
+    const [key, value] = cookie.split('=');
+    if (key === cookieID)
+    {
+      return value;
+    }
+  }
+  return null; // Return null if the cookie is not found
 }
